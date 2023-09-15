bf578cca5815ec14baa828a800e30a3fa9e094df
eduardo <dudu.alcantaraa@gmail.com>
2023-09-15 15:07:37 -0300

    V1.0.0

---
+++ src/components/billing-value/billing-value-paid/index.jsx
@@ -0,0 +1,21 @@
+import './style.css';
+import payedIcon from '../../../assets/Payed-icon.png'
+
+
+function BillingValuePaid() {
+  return (
+    <div className='billing-value'>
+      <div className='billing-value-icon'>
+        <img src={payedIcon} alt='Ícone de confirmação de pagamento'></img>
+      </div>
+
+      <div className='billing-value-content'>
+        <p className='billing-value-title'>Cobranças Pagas</p>
+        <p className='billing-value-value'>R$ 30.000</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default BillingValuePaid;
