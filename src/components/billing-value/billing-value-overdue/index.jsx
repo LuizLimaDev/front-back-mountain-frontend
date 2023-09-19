130181b1bffeb5a60e0cb7fd5c66bf83d5660937
Fernanda Campelo <fernanda.cstdias@gmail.com>
2023-09-19 00:15:59 -0300

    corrigindo formatação dos valores monetários para BRL

--- src/components/billing-value/billing-value-overdue/index.jsx
+++ src/components/billing-value/billing-value-overdue/index.jsx
@@ -1,23 +1,25 @@
-import './style.css';
-import overdueIcon from '../../../assets/Nonpayed-icon.png'
-import useMetrics from '../../../hooks/useMetricsDashboard';
-
+import "./style.css";
+import overdueIcon from "../../../assets/Nonpayed-icon.png";
+import useMetrics from "../../../hooks/useMetricsDashboard";
+import { moneyFormat } from "../../../utils/moneyFormat";
 
 function BillingValueOverdue() {
-  const  { metrics } = useMetrics();
-  return (
-    <div className='billing-value-overdue'>
-      <div className='billing-value-icon'>
-        <img src={overdueIcon} alt='Ícone de pagamento vencido'></img>
-      </div>
+	const { metrics } = useMetrics();
 
-      <div className='billing-value-content'>
-        <p className='billing-value-title'>Cobranças Vencidas</p>
-        <p className='billing-value-value'>R$ {metrics.totalPrices.valueOverdue}</p>
-      </div>
+	return (
+		<div className="billing-value-overdue">
+			<div className="billing-value-icon">
+				<img src={overdueIcon} alt="Ícone de pagamento vencido"></img>
+			</div>
 
-    </div>
-  )
+			<div className="billing-value-content">
+				<p className="billing-value-title">Cobranças Vencidas</p>
+				<p className="billing-value-value">
+					{moneyFormat.format(metrics.totalPrices.valueOverdue)}
+				</p>
+			</div>
+		</div>
+	);
 }
 
 export default BillingValueOverdue;
---
+++ src/utils/moneyFormat.jsx
@@ -0,0 +1,5 @@
+export const moneyFormat = new Intl.NumberFormat("BRL", {
+	style: "currency",
+	currency: "BRL",
+	minimumFractionDigits: 2,
+});
