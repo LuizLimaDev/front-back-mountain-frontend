fb93e0bbbd0b385664cf1bb1989f7a7f52e78d43
Fernanda Campelo <fernanda.cstdias@gmail.com>
2023-09-25 20:26:10 -0300

    fix: consertar estilizações das tabelas

--- src/components/page-clients/clients-search/index.jsx
+++ src/components/page-clients/clients-search/index.jsx
@@ -40,7 +40,11 @@ function ClientHeader() {
 					</button>
 					<form onSubmit={handleSubmit}>
 						<OutlinedInput
-							sx={{ padding: "0" }}
+							sx={{
+								padding: "0",
+								borderRadius: ".625rem",
+								filter: "drop-shadow(7px 7px 4px rgba(218, 1, 117, 0.05))",
+							}}
 							id="search-input"
 							className="client-input"
 							type="search"
