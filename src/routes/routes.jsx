16aac2b37c0e56e88dd666e5b5ce196d286971ba
Amandalfs <amandalfs57@gmail.com>
2023-09-25 16:14:27 -0300

    fix: arrumar rotas protegidas

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -11,31 +11,59 @@ import Clients from '../pages/Clients/Clients';
 import HomeLayout from "../components/HomeLayout";
 import Billings from "../pages/Billings";
 import ClientDetailed from "../pages/ClientDetailed/ClientDetailed";
-
+import api from "../services/api";
+import { useEffect } from "react";
 
 // eslint-disable-next-line react/prop-types
-function ProtectedRoute({ isProtected, url }) {
+function ProtectedRouteByLogout({ isProtected, url }) {
+  return isProtected ? <Outlet /> : <Navigate to={url} />;
+}
+
+// eslint-disable-next-line react/prop-types
+function ProtectedRouteByLogged({ isProtected, url, remove }) {
+  useEffect(()=>{
+    const fetchData = async () => {
+      try {
+        await api.get("/users/profile", {
+          headers: {
+            Authorization: `Bearer ${isProtected}`
+          }
+        });
+      } catch (error) {
+        // Se ocorrer um erro ao buscar o perfil do usuário, redirecione para a rota especificada
+        remove("token");
+      }
+    };
+
+    if (isProtected) {
+      fetchData();
+    }
+  },[isProtected, remove]);
+
   return isProtected ? <Outlet /> : <Navigate to={url} />;
 }
 
 function MainRoutes() {
-  const { value } = useContext(SingContext);
+  const { value, remove } = useContext(SingContext);
 
 
   return (
     <Routes>
+
       <Route
-        element={<ProtectedRoute isProtected={!value} url={"/home"} />}
+        element={<ProtectedRouteByLogout isProtected={!value} url={"/home"} />}
       >
+
         <Route element={<RegisterLayout />}>
           <Route path="/singup" element={<SingUp />} />
           <Route path="/password" element={<Password />} />
           <Route path="/success" element={<Success />} />
         </Route>
         <Route path="/" element={<SingIn />} />
+
       </Route>
 
-      <Route element={<ProtectedRoute isProtected={value} url={"/"} />}>
+      <Route element={<ProtectedRouteByLogged isProtected={value} url={"/"} remove={remove} />}>
 
         <Route element={<HomeLayout />} >
           <Route path="/home" element={<Home />} />
@@ -43,7 +71,9 @@ function MainRoutes() {
           <Route path="/clients/:customerId" element={<ClientDetailed />} />
           <Route path="/billings" element={<Billings />} />
         </Route>
+        
       </Route>
+
     </Routes >
   );
 }
