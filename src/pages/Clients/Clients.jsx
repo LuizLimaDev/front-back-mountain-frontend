c1b1eb17befcff12baf82b74a29dec87ac01d92c
Fernanda Campelo <fernanda.cstdias@gmail.com>
2023-09-26 10:08:12 -0300

    fix: adicionar padding bottom na página de clientes

--- src/pages/Clients/Clients.jsx
+++ src/pages/Clients/Clients.jsx
@@ -1,43 +1,43 @@
-import ClientsTable from '../../components/page-clients/clients-table';
-import ClientHeader from '../../components/page-clients/clients-search';
+import ClientsTable from "../../components/page-clients/clients-table";
+import ClientHeader from "../../components/page-clients/clients-search";
 import ClientModal from "../../components/ClientModal";
-import SnackBar from '../../components/SnackBar';
-import './style.css';
-import { Box } from '@mui/material';
-import { useTheme } from '@emotion/react';
-import { useContext } from 'react';
-import { ModalsContext } from '../../context/ModalsContext';
+import SnackBar from "../../components/SnackBar";
+import "./style.css";
+import { Box } from "@mui/material";
+import { useTheme } from "@emotion/react";
+import { useContext } from "react";
+import { ModalsContext } from "../../context/ModalsContext";
 
 function Clients() {
-    const theme = useTheme()
-    const {
-        openSnackChargeAdd, setOpenSnackChargeAdd,
-        openSnackClientAdd, setOpenSnackClientAdd
-    } = useContext(ModalsContext)
+	const theme = useTheme();
+	const {
+		openSnackChargeAdd,
+		setOpenSnackChargeAdd,
+		openSnackClientAdd,
+		setOpenSnackClientAdd,
+	} = useContext(ModalsContext);
 
-    return (
-        <Box
-            sx={theme.layoutOutletHome}
-        >
-            <div>
-                <div>
-                    <ClientHeader />
-                    <ClientsTable />
-                    <ClientModal />
-                    <SnackBar
-                        phrase={"Cadastro concluído com sucesso"}
-                        openSnack={openSnackClientAdd}
-                        setOpenSnack={setOpenSnackClientAdd}
-                    />
-                    <SnackBar
-                        phrase={"Cobrança cadastrada com sucesso"}
-                        openSnack={openSnackChargeAdd}
-                        setOpenSnack={setOpenSnackChargeAdd}
-                    />
-                </div>
-            </div>
-        </Box>
-    );
+	return (
+		<Box sx={{ ...theme.layoutOutletHome, paddingBottom: "3rem" }}>
+			<div>
+				<div>
+					<ClientHeader />
+					<ClientsTable />
+					<ClientModal />
+					<SnackBar
+						phrase={"Cadastro concluído com sucesso"}
+						openSnack={openSnackClientAdd}
+						setOpenSnack={setOpenSnackClientAdd}
+					/>
+					<SnackBar
+						phrase={"Cobrança cadastrada com sucesso"}
+						openSnack={openSnackChargeAdd}
+						setOpenSnack={setOpenSnackChargeAdd}
+					/>
+				</div>
+			</div>
+		</Box>
+	);
 }
 
 export default Clients;
