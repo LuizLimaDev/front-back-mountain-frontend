b2e34cb52b495789a35cb16322a9674c9e6546e3
Luiz Lima <devluizlima@gmail.com>
2023-09-28 20:08:51 -0300

    Update ChargesContext.jsx
    Correção no export.

--- src/context/ChargesContext.jsx
+++ src/context/ChargesContext.jsx
@@ -43,7 +43,7 @@ export const ChargesProvider = ({ children }) => {
 				charge,
 				setCharge,
 				chargeDetailSelected,
-				setChargeDetailSelected
+				setChargeDetailSelected,
 				chargeEdit, 
 				setChargeEdit,
 				chargesParams,
