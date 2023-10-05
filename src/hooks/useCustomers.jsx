70d0508dcc6a132d4554f703015285068e4d2d03
Fernanda Campelo <fernanda.cstdias@gmail.com>
2023-10-05 17:18:03 -0300

    importando setCustomers no hook

--- src/hooks/useCustomers.jsx
+++ src/hooks/useCustomers.jsx
@@ -1,76 +1,79 @@
-import { useContext, useEffect } from "react"
-import api from "../services/api"
+import { useContext, useEffect } from "react";
+import api from "../services/api";
 import { SingContext } from "../context/SingContext";
 import { CustomersContext } from "../context/CustomersContext";
 
 export default function useCustomers() {
-  const {         
-   customers,
-   customer,
-   setCustomers,
-   setCustomer,
-   formCustomer,
-   setFormCustomer,
-   chargesByCustomer, 
-   setChargesByCustomer,
-   customersParams,
-   setCustomersParams
-  } = useContext(CustomersContext);
-  const {  value } = useContext(SingContext);
+	const {
+		customers,
+		customer,
+		setCustomers,
+		setCustomer,
+		formCustomer,
+		setFormCustomer,
+		chargesByCustomer,
+		setChargesByCustomer,
+		customersParams,
+		setCustomersParams,
+	} = useContext(CustomersContext);
+	const { value } = useContext(SingContext);
 
-  async function customersUpdate(){
-    try {
-        const { data } = await api.get("/customers", {
-            headers: {
-                Authorization: `Bearer ${value}`
-            },
-            params: {
-              ...customersParams
-            }
-        });
-        setCustomers(data.customers);
-    } catch (error) {
-        console.log(error);
-    }
-  }
+	async function customersUpdate() {
+		try {
+			const { data } = await api.get("/customers", {
+				headers: {
+					Authorization: `Bearer ${value}`,
+				},
+				params: {
+					...customersParams,
+				},
+			});
+			setCustomers(data.customers);
+		} catch (error) {
+			console.log(error);
+		}
+	}
 
-  async function getCustomer(customerId){
-    try {
-      const { data } = await api.get(`/customers/${customerId}`, {
-        headers: {
-            Authorization: `Bearer ${value}`
-        }
-      });
+	async function getCustomer(customerId) {
+		try {
+			const { data } = await api.get(`/customers/${customerId}`, {
+				headers: {
+					Authorization: `Bearer ${value}`,
+				},
+			});
 
-      const { data: charges } = await api.get(`/customers/${customerId}/charges`, {
-        headers: {
-            Authorization: `Bearer ${value}`
-        }
-      });
-      setCustomer(data.detailsCustomer[0]);
-      setFormCustomer(data.detailsCustomer[0]);
-      setChargesByCustomer(charges.detailsCustomerCharges);
-    } catch (error) {
-      console.log("api error", error);
-    }
-    
-  }
-  
-  useEffect(()=>{
-   customersUpdate();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [customersParams])
+			const { data: charges } = await api.get(
+				`/customers/${customerId}/charges`,
+				{
+					headers: {
+						Authorization: `Bearer ${value}`,
+					},
+				}
+			);
+			setCustomer(data.detailsCustomer[0]);
+			setFormCustomer(data.detailsCustomer[0]);
+			setChargesByCustomer(charges.detailsCustomerCharges);
+		} catch (error) {
+			console.log("api error", error);
+		}
+	}
 
-  return {
-    customers,
-    customersUpdate,
-    getCustomer,
-    customer,
-    setCustomer,
-    formCustomer,
-    setFormCustomer,
-    chargesByCustomer,
-    customersParams,
-    setCustomersParams
-  }
+	useEffect(() => {
+		customersUpdate();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [customersParams]);
+
+	return {
+		customers,
+		setCustomers,
+		customersUpdate,
+		getCustomer,
+		customer,
+		setCustomer,
+		formCustomer,
+		setFormCustomer,
+		chargesByCustomer,
+		customersParams,
+		setCustomersParams,
+	};
 }
